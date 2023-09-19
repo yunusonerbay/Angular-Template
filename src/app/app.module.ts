f3193fe52de139b874db014acdcae7e8d4ddde2c
yunus <onerbayyunus@gmail.com>
2023-09-19 22:45:17 +0300

    toastr talwinds css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-
-import { APP_INITIALIZER,NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { ToastrModule } from 'ngx-toastr';
-import { registerLocaleData, PathLocationStrategy, LocationStrategy } from '@angular/common';
+import {
+  registerLocaleData,
+  PathLocationStrategy,
+  LocationStrategy,
+} from '@angular/common';
 import en from '@angular/common/locales/en';
 
 import { AppRoutingModule } from './app-routing.module';
 import { TemplateModule } from './shared/template/template.module';
 import { SharedModule } from './shared/shared.module';
 
-import { NgChartsModule  } from 'ng2-charts';
-import { NgApexchartsModule } from "ng-apexcharts";
+import { NgChartsModule } from 'ng2-charts';
+import { NgApexchartsModule } from 'ng-apexcharts';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppComponent } from './app.component';
@@ -30,60 +33,61 @@ import { HttpErrorHandlerInterceptorService } from './shared/services/http-error
 
 registerLocaleData(en);
 
-export function setupTranslateFactory(
-  service: LanguageService): Function {
+export function setupTranslateFactory(service: LanguageService): Function {
   return () => service.use('tr');
 }
 @NgModule({
-    declarations: [
-        AppComponent,
-        CommonLayoutComponent,
-        FullLayoutComponent,
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        AppRoutingModule,
-        TemplateModule,
-        SharedModule,
-        NzBreadCrumbModule,
-        NzSpinModule,
-        NgChartsModule,
-        NgApexchartsModule,
-        ToastrModule.forRoot({
-          }),
-        AngularSvgIconModule.forRoot()
-
-    ],
-    providers: [
-        {
-            provide: NZ_I18N,
-            useValue: en_US,
-        },
-        {
-            provide: LocationStrategy,
-            useClass: PathLocationStrategy
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: setupTranslateFactory,
-            deps: [ LanguageService ],
-            multi: true
-          },
-          {
-            provide:HTTP_INTERCEPTORS,
-             useClass:AuthInterceptor,  
-             multi:true
-            },
-            {
-                provide: HTTP_INTERCEPTORS,
-                useClass: HttpErrorHandlerInterceptorService,
-                multi: true,
-              },
-        ThemeConstantService,
-        AuthenticationService,
-      
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, CommonLayoutComponent, FullLayoutComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TemplateModule,
+    SharedModule,
+    NzBreadCrumbModule,
+    NzSpinModule,
+    NgChartsModule,
+    NgApexchartsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right', // Toastr'ın görüntüleneceği pozisyonu ayarlayın
+      preventDuplicates: true, // Tekrar eden mesajları önleyin
+      closeButton: true, // Kapatma düğmesini gösterin
+      timeOut: 30000, // Mesajın otomatik olarak kaybolma süresini ayarlayın
+      progressBar: true, // İlerleme çubuğunu gösterin
+      toastClass: 'ngx-toastr', // Toastr için özel stil sınıfını belirtin
+      titleClass: 'toast-title', // Başlık için özel stil sınıfını belirtin
+      messageClass: 'toast-message', // Mesaj için özel stil sınıfını belirtin
+    }),
+    AngularSvgIconModule.forRoot(),
+  ],
+  providers: [
+    {
+      provide: NZ_I18N,
+      useValue: en_US,
+    },
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: setupTranslateFactory,
+      deps: [LanguageService],
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorHandlerInterceptorService,
+      multi: true,
+    },
+    ThemeConstantService,
+    AuthenticationService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/alert/description.ts
+++ src/app/components/alert/description.ts
@@ -3,12 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nz-demo-alert-description',
   template: `
-    <nz-alert 
-    class="[&>.ant-alert]:bg-success/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-success [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]" 
-    nzMessage="Success Text" 
-    nzDescription="Success Description Success Description Success Description">
+    <nz-alert
+      *ngFor="let alert of alerts"
+      class="{{ alert.classes }}"
+      nzMessage="{{ alert.message }}"
+      nzDescription="{{ alert.description }}"
+    >
     </nz-alert>
-    <nz-alert class="[&>.ant-alert]:bg-info/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-info [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]" nzMessage="Info Text" nzDescription="Info Description Info Description Info Description Info Description">
+
+    <!-- <nz-alert
+      class="[&>.ant-alert]:bg-success/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-success [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]"
+      nzMessage="Success Text"
+      nzDescription="Success Description Success Description Success Description"
+    >
+    </nz-alert>
+    <nz-alert
+      class="[&>.ant-alert]:bg-info/10 [&>.ant-alert]:border-none
+       [&>.ant-alert>div>span]:text-info [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]"
+      nzMessage="Info Text"
+      nzDescription="Info Description Info Description Info Description Info Description"
+    >
     </nz-alert>
     <nz-alert
       class="[&>.ant-alert]:bg-warning/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-warning [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]"
@@ -16,15 +30,53 @@ import { Component } from '@angular/core';
       nzDescription="Warning Description Warning Description Warning Description Warning Description"
     >
     </nz-alert>
-    <nz-alert class="[&>.ant-alert]:bg-danger/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-danger [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px] mb-0" nzMessage="Error Text" nzDescription="Error Description Error Description Error Description Error Description">
-    </nz-alert>
+    <nz-alert
+      class="[&>.ant-alert]:bg-danger/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-danger [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px] mb-0"
+      nzMessage="Error Text"
+      nzDescription="Error Description Error Description Error Description Error Description"
+    >
+    </nz-alert> -->
   `,
   styles: [
     `
       nz-alert {
         margin-bottom: 16px;
       }
-    `
-  ]
+    `,
+  ],
 })
-export class NzDemoAlertDescriptionComponent {}
+export class NzDemoAlertDescriptionComponent {
+  alerts: AlertModel[] = [];
+  constructor() {
+    // Örnek alert öğelerini diziye ekleyin
+    this.alerts.push(
+      new AlertModel(
+        '[&>.ant-alert]:bg-success/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-success [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]',
+        'Success Text',
+        'Success Description Success Description Success Description'
+      ),
+      new AlertModel(
+        '[&>.ant-alert]:bg-info/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-info [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]',
+        'Info Text',
+        'Info Description Info Description Info Description Info Description'
+      ),
+      new AlertModel(
+        '[&>.ant-alert]:bg-warning/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-warning [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px]',
+        'Warning Text',
+        'Warning Description Warning Description Warning Description Warning Description'
+      ),
+      new AlertModel(
+        '[&>.ant-alert]:bg-danger/10 [&>.ant-alert]:border-none [&>.ant-alert>div>span]:text-danger [&>.ant-alert]:px-[20px] [&>.ant-alert]:py-[16.5px] [&>.ant-alert]:rounded-4 capitalize text-[15px] mb-0',
+        'Error Text',
+        'Error Description Error Description Error Description Error Description'
+      )
+    );
+  }
+}
+export class AlertModel {
+  constructor(
+    public classes: string,
+    public message: string,
+    public description: string
+  ) {}
+}
